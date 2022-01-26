ba5215464ba13514e2c0f9065eb8d18326f5d25a
Erik Ahumada <costerik@hotmail.com>
2022-01-26 16:20:19 -0500

    [NUF-007-alert-component] alert component created

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export {default as Header, MemoizedHeader} from './header';
 export {default as ActionButtons, MemoizedActionButtons} from './action-buttons';
 export {default as List, MemoizedList} from './list';
 export {default as Board, MemoizedBoard} from './board';
+export {default as Alert, MemoizedAlert} from './alert';
