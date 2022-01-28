64d97c14ec76607ade3fc26fcf61321daf0367f7
Erik Ahumada <costerik@hotmail.com>
2022-01-28 11:32:42 -0500

    [NUF-008-containers-component] components exports updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,13 @@
-export {default as Card, MemoizedCard} from './card';
-export {default as Header, MemoizedHeader} from './header';
-export {default as ActionButtons, MemoizedActionButtons} from './action-buttons';
-export {default as List, MemoizedList} from './list';
-export {default as Board, MemoizedBoard} from './board';
-export {default as Alert, MemoizedAlert} from './alert';
+export {default as Card} from './card';
+export {default as Header} from './header';
+export {default as ActionButtons} from './action-buttons';
+export {default as List} from './list';
+export {default as Board} from './board';
+export {default as Alert} from './alert';
+
+export * from './card';
+export * from './header';
+export * from './action-buttons';
+export * from './list';
+export * from './board';
+export * from './alert';
